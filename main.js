7b5bb47959ec196c244e95a3b3849a18b282330f
Asher Ferri <asher@Ashers-MacBook-Pro.local>
2020-08-26 13:25:08 -0400

    conflicts settled \m/

--- main.js
+++ main.js
@@ -18,3 +18,10 @@ function difference (num1, num2) {
 
 console.log(`10 - 5 = ${difference(10,5)}`)
 
+/**
+ * git add -A
+ * git commit -m "cowabunga"
+ * git push - file updte local branch
+ * git push origin your_branch .pushes branch live.
+ */
+
